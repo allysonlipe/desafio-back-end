c234ff49ccf5d169b9911c2af02dbf14b67d470c
allysonlipe <allysongunner1@hotmail.com>
2024-08-02 14:26:11 -0300

    criacao do repositorio

---
+++ README.md
@@ -0,0 +1,119 @@
+# desafio-dev-backend
+
+## Descrição
+
+Estou criando esse repositório para um teste técnico para uma vaga de Beck-end, tendo como objetivo: 
+```
+"Criar uma aplicação CRUD básica com banco de dados e API em Python..." 
+```
+## Resultados Esperados
+
+```
+"Ao final deste desafio, deve existir uma aplicação backend capaz de criar , atualizar , remover e selecionar dados de um banco de dados."
+```
+
+
+## Funcionalidades
+
+Liste as principais funcionalidades do projeto:
+
+- Criar dados em um banco de dados;
+- Atualizar dados em um banco de dados;
+- Remover dados em um banco de dados;
+- Selecionar dados em um banco de dados.
+
+## Tecnologias Utilizadas
+
+Mencione as principais tecnologias, bibliotecas e frameworks utilizados no projeto.
+
+- Python (Framework Django)
+- MYSQL
+
+## Requisitos
+
+Liste os pré-requisitos necessários para configurar e executar o projeto.
+
+- Python 3.x
+- Django 5.x
+- MySQL 8.x
+
+## Instalação
+
+Passo a passo de como configurar o ambiente de desenvolvimento e instalar as dependências.
+
+1. Clone o repositório:
+   ```bash
+   git clone https://github.com/allysonlipe/desafio-DEV-BackEnd.git
+   ```
+
+2. Navegue até o diretório do projeto:
+   ```bash
+   cd desafio-DEV-BackEnd
+   ```
+
+3. Crie e ative um ambiente virtual:
+   ```bash
+   python -m venv myenv
+   source myenv/bin/activate  # No Windows: . myenv\Scripts\activate
+   ```
+   Caso encontre o seguinte erro:
+   ```
+   não pode ser carregado porque a execução de scripts foi desabilitada neste sistema.     
+   Para obter mais informações, consulte about_Execution_Policies em 
+   https://go.microsoft.com/fwlink/?LinkID=135170.
+   No linha:1 caractere:3
+   ```
+   1. **Abra o PowerShell como Administrador;**
+   &nbsp;
+   2. **Verifique a política atual (opcional):**
+      ```
+      Get-ExecutionPolicy
+      ```
+   3. **Altere a política de execução para permitir scripts:**
+      ```
+      Set-ExecutionPolicy RemoteSigned
+      ```
+   4. **Confirme a alteração:**
+         Quando solicitado, pressione Y e depois Enter para confirmar a alteração.
+   5. **Tente ativar o ambiente virtual novamente:**
+         ```
+         source myenv/bin/activate  # No Windows: myenv\Scripts\activate
+         ```
+
+
+4. Para instalar todas as dependências, execute:
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+## Configuração
+
+Explique como configurar as variáveis de ambiente e outras configurações necessárias.
+
+1. Dentro do seu projeto, há um arquivo chamado settings.py, procure por **DATABASES** e faça sua configuração, se você utilizar meus scripts SQLS a configuração será:
+
+   ```env
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'desafio',
+        'USER':'root',
+        'PASSWORD':'admin',
+        'HOST': 'localhost',
+        'PORT':'3306',
+    }
+      ```
+
+## Uso
+
+Instruções sobre como executar o projeto.
+
+1. Inicie o servidor:
+   ```bash
+      VER DEPOIS
+   ```
+
+2. Acesse a aplicação em `http://localhost:8000`.
+
+## Licença
+
+This project is under MIT license. See the [LICENSE](LICENSE.md) for more details.
---
+++ banco-de-dados/desafio.sql
@@ -0,0 +1,157 @@
+-- Objetivo : Criar estruturas de tabelas e procedures no banco de dados.
+CREATE DATABASE IF NOT EXISTS desafio; 
+USE desafio;
+
+-- Criar script para criação da tabela Pessoas:
+CREATE TABLE IF NOT EXISTS pessoas (
+    id_pessoa INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    data_nascimento DATE NOT NULL,
+    salario DECIMAL(10, 2) NOT NULL,
+    observacoes TEXT
+);
+
+-- Criar script para alteração da tabela Pessoas:
+ALTER TABLE pessoas
+ADD COLUMN nome_mae VARCHAR(100),
+ADD COLUMN nome_pai VARCHAR(100),
+ADD COLUMN cpf VARCHAR(11);
+
+-- Criar um script para adicionar índices de pesquisa a tabela Pessoas (nome e data_nascimento):
+CREATE INDEX idx_nome ON pessoas (nome);
+CREATE INDEX idx_data_nascimento ON pessoas (data_nascimento);
+
+-- Criar um script para adicionar indice/chave única a tabela Pessoas: Chave Única:cpf
+ALTER TABLE pessoas
+ADD CONSTRAINT unique_cpf UNIQUE (cpf);
+
+/* Criar um script para criar uma procedure de inserção de registros:   
+Procedure deverá receber como parâmetros os campos da tabela Pessoas 
+Procedure deverá retomar para a API o valor do campo id_pessoa. */
+
+DELIMITER $$
+
+CREATE PROCEDURE inserir_pessoa (
+	
+    IN p_nome VARCHAR(100),
+	IN p_data_nascimento DATE,
+	IN p_salario DECIMAL(10,2),
+	IN p_observacoes TEXT,
+	IN p_nome_mae VARCHAR(100),
+	IN p_nome_pai VARCHAR(100),
+	IN p_cpf VARCHAR(11),
+	OUT p_id_pessoa INT
+
+)
+
+BEGIN
+	INSERT INTO pessoas (nome, data_nascimento, salario, observacoes, nome_mae, nome_pai, cpf)
+	VALUES (p_nome, p_data_nascimento, p_salario, p_observacoes, p_nome_mae, p_nome_pai, p_cpf );
+    SET p_id_pessoa = LAST_INSERT_ID();
+    SELECT p_id_pessoa AS id_pessoa;
+END $$
+
+DELIMITER ;
+
+/*
+Criar um script para criar uma procedure de atualização de registros:
+Procedure deverá receber como parâmetros os campos da tabela Pessoas .
+Procedure deverá retomnar para a API o valor OK
+*/
+
+DELIMITER $$
+
+CREATE PROCEDURE atualizar_pessoa (
+    IN p_id_pessoa INT,
+    IN p_nome VARCHAR(100),
+    IN p_data_nascimento DATE,
+    IN p_salario DECIMAL(10, 2),
+    IN p_observacoes TEXT,
+    IN p_nome_mae VARCHAR(100),
+    IN p_nome_pai VARCHAR(100),
+    IN p_cpf VARCHAR(11)
+)
+BEGIN
+    UPDATE pessoas
+    SET 
+        nome = p_nome,
+        data_nascimento = p_data_nascimento,
+        salario = p_salario,
+        observacoes = p_observacoes,
+        nome_mae = p_nome_mae,
+        nome_pai = p_nome_pai,
+        cpf = p_cpf
+    WHERE id_pessoa = p_id_pessoa;
+
+    SELECT 'OK' AS status;
+END $$
+
+DELIMITER ;
+
+/* Criar um script para criar uma procedure de remoção de registros:
+Procedure deverá receber como parâmetros o id_pessoa a ser removido da tabela de Pessoas .
+Procedure deverá retornar para a API o valor OK */
+
+DELIMITER $$
+
+CREATE PROCEDURE deletar_pessoa (
+    IN p_id_pessoa INT
+)
+BEGIN
+    DELETE FROM pessoas
+    WHERE id_pessoa = p_id_pessoa;
+
+    SELECT 'OK' AS status;
+END $$
+
+DELIMITER ;
+
+
+/* Criar um script para criar uma procedure de seleção de todos os registros:
+Procedure deverá retornar todos os registros existentes na tabela de Pessoas */
+
+DELIMITER $$
+
+CREATE PROCEDURE selecionar_tudo (
+
+)
+BEGIN
+	SELECT * FROM pessoas;
+END $$
+
+DELIMITER ;
+
+/* Criar um script para criar uma procedure de obter um registro:
+Procedure deverá receber como parâmetros o id_pessoa a ser selecionado na tabela. */
+
+DELIMITER $$
+
+CREATE PROCEDURE selecionar_pessoa(
+IN p_id_pessoa INT
+)
+
+BEGIN
+	SELECT * FROM pessoas
+    WHERE id_pessoa = p_id_pessoa;
+END $$
+
+DELIMITER ;
+
+SET @id_pessoa = 0;
+
+/*Criar um script para inserir dados na tabela de Pessoas através da procedure de inserção .*/
+CALL inserir_pessoa('João da Silva','1990-05-15',2500.00,'esse é o João','Maria da Silva','José da Silva','12345678906',@id_pessoa);
+
+/*Criar um script para atualizar dados na tabela de Pessoas através da procedure de atualização*/
+call desafio.atualizar_pessoa(@id_pessoa, 'José da Silva Júnior', '2002-12-02', 800.00, 'esse agora é o Filho do José', 'Francisca da Silva', 'José da Silva', '12345678902');
+
+/*Criar um script para remover dados na tabela de Pessoas através da procedure de remoção . */
+call desafio.deletar_pessoa(@id_pessoa);
+
+/*Criar um script para selecionar registros na tabela de Pessoas através da procedure de seleção de todos os registros.*/
+call desafio.selecionar_tudo();
+
+/*Criar um script para selecionar um registro na tabela de Pessoas através da procedure de seleção de um registro.*/
+call desafio.selecionar_pessoa(@id_pessoa);
+
+
